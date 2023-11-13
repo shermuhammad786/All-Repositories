dd9167199561fc099c33bf9e9eacd93721ccf1fd
shermuhammad786 <sherabro141@gmail.com>
2023-11-13 16:48:32 +0500

    background Color anination

--- guess my number/app.js
+++ guess my number/app.js
@@ -5,23 +5,23 @@ var score = document.getElementById("score")
 var mainDiv = document.getElementById("mainDiv")
 var start = document.getElementById("start");
 var qMark = document.getElementById("markq");
-var randomNumebr = Math.floor(Math.random()*20+1)
-button.addEventListener("click",function (){
-    if(input.value == randomNumebr){
+var randomNumebr = Math.floor(Math.random() * 20 + 1)
+button.addEventListener("click", function () {
+    if (input.value == randomNumebr) {
         mainDiv.style.backgroundColor = "green";
         start.innerHTML = "Congratulations! You Win";
         qMark.innerHTML = randomNumebr;
         score.innerHTML = +score.innerHTML + 10;
         highscore.innerHTML = +highscore.innerHTML + +score.innerHTML
-    }else if(input.value < randomNumebr){
+    } else if (input.value < randomNumebr) {
         start.innerHTML = "Too low"
         score.innerHTML = +score.innerHTML - 1;
-    }else{
+    } else {
         score.innerHTML = +score.innerHTML - 1;
         start.innerHTML = "Too high"
     }
 })
 var again = document.getElementById("again");
-again.addEventListener("click",function(){
+again.addEventListener("click", function () {
     window.location.reload()
 })
